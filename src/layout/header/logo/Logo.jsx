ce39816a90648724e1eb44db620b510fb6f2101e
QBrain LTD <140598320+QbrainLTD@users.noreply.github.com>
2025-01-12 20:25:04 +0200

    newCommit

--- src/layout/header/logo/Logo.jsx
+++ src/layout/header/logo/Logo.jsx
@@ -8,7 +8,8 @@ export default function Logo() {
   return (
     <>
       <NavBarLink to={ROUTES.ROOT}>
-        <img src="/images/ksheraLogo.png" alt="KsheraIcon" style={{ width: "100%", height: "3vh" }} />
+        <Typography sx={{fontFamily:"assitant", color:"white", fontSize:"28pt"}}>כשרה</Typography>
+        {/* <img src="/images/ksheraLogo.png" alt="KsheraIcon" style={{ width: "100%", height: "3vh" }} /> */}
       </NavBarLink>
     </>
   );
